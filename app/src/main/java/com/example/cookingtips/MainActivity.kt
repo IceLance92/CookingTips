9831688af19842f27f96c46b5750787519c9bc07
Aleksandr <mograin_92@mail.ru>
2025-08-12 03:02:21 +0300

    создание ÐдругогÐо функционала

--- app/src/main/java/com/example/cookingtips/MainActivity.kt
+++ app/src/main/java/com/example/cookingtips/MainActivity.kt
@@ -6,11 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.cookingtips.model.ReceptRepository
 import com.example.cookingtips.ui.theme.CookingTipsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CookingTipsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CookingTipsApp(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    CookingTipsApp()
+                }
                 }
             }
         }
     }
-}
 
 @Composable
-fun CookingTipsApp(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun CookingTipsApp() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar()
+        }
+    ) { val recepts = ReceptRepository.recepts
+        ReceptList(recepts = recepts, contentPadding = it)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge,
+            )
+        },
         modifier = modifier
     )
 }
-
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    CookingTipsTheme {
-        CookingTipsApp("Android")
+fun SuperReceptsPreview() {
+    CookingTipsTheme(darkTheme = false) {
+        CookingTipsApp()
+    }
+}
+
+@Preview
+@Composable
+fun SuperReceptsDarkThemePreview() {
+    CookingTipsTheme(darkTheme = true) {
+        CookingTipsApp()
     }
 }
--- app/src/main/java/com/example/cookingtips/model/ReceptDataSource.kt
+++ app/src/main/java/com/example/cookingtips/model/ReceptDataSource.kt
@@ -7,47 +7,47 @@ object ReceptRepository {
         Recept(
             nameRes = R.string.recept1,
             descriptionRes = R.string.description1,
-            imageRes = R.drawable.салат
+            imageRes = R.drawable.salat
         ),
         Recept(
             nameRes = R.string.recept2,
             descriptionRes = R.string.description2,
-            imageRes = R.drawable.зразы
+            imageRes = R.drawable.zrazi
         ),
         Recept(
             nameRes = R.string.recept3,
             descriptionRes = R.string.description3,
-            imageRes = R.drawable.бутерброды
+            imageRes = R.drawable.buteri
         ),
         Recept(
             nameRes = R.string.recept4,
             descriptionRes = R.string.description4,
-            imageRes = R.drawable.отбивные
+            imageRes = R.drawable.otbivnii
         ),
         Recept(
             nameRes = R.string.recept5,
             descriptionRes = R.string.description5,
-            imageRes = R.drawable.тефтели
+            imageRes = R.drawable.tefteli
         ),
         Recept(
             nameRes = R.string.recept6,
             descriptionRes = R.string.description6,
-            imageRes = R.drawable.колбаса
+            imageRes = R.drawable.kolbasa
         ),
         Recept(
             nameRes = R.string.recept7,
             descriptionRes = R.string.description7,
-            imageRes = R.drawable.блины
+            imageRes = R.drawable.blini
         ),
         Recept(
             nameRes = R.string.recept8,
             descriptionRes = R.string.description8,
-            imageRes = R.drawable.курники
+            imageRes = R.drawable.kurniki
         ),
         Recept(
             nameRes = R.string.recept9,
             descriptionRes = R.string.description9,
-            imageRes = R.drawable.суп
+            imageRes = R.drawable.sup
         ),
     )
 }
